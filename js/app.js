d7796e4c2e975de5ea76e1a2b13dd18e65b77a5f
Mark Cronje <markcronje@Marks-MacBook-Air.local>
2016-05-09 11:54:09 +1000

    add CSS styling

--- js/app.js
+++ js/app.js
@@ -1,64 +1,16 @@
-var rowCss = {
-  height: '200px',
-  display: 'flex',
-  width: '1000px',
-  height: '200px',
-  textAlign: 'center',
-  margin: ' 0 auto'
-}
+var board = $("<div id='board'></div>");
+$('body').append(board);
 
-var colCss = {
-  width:'200px',
-  height: '200px',
-  borderStyle: 'solid',
-  borderColor: 'black',
-  textAlign: 'center',
-  margin: ' 0 auto'
-}
-
-var bodyStyle = {
-  textAlign: 'center',
-  margin: ' 0 auto'
+// creating a loop to automate the process of creating a table of 3 rows by 3 columns using $jQuery$
+for (var rowNo = 0; rowNo <= 2; rowNo++) {
+  $('<div>', {'class':'row-' + rowNo}).appendTo('#board')
+  for (var colNo = 0; colNo <= 2; colNo++) {
+    $('<div>', {'class':'column ' + colNo}).appendTo('.row-'+rowNo)
+  }
 }
 
 
-var boardCenter = {
-  width: '600px',
-  height: '600px',
-  borderStyle: 'dotted',
-  borderColor: 'black',
-  margin: '0 auto',
-}
-
-
-$('body').css(bodyStyle);
-$("#board").css(boardCenter);
-
-$('<h1>').appendTo('body').text("Welcome to Tick Tac Toe!");
-$('<h2>').appendTo('body').text("Click on the square!");
-$('<div>', {'id':'board'}).appendTo('body');
-$('<div>', {'id':'winner'}).appendTo('body');
-$('<h1>').appendTo('#winner').text("Winner");
-
-
-
-// $(''#board').each(function(i, e) {
-
-$('<div>', {'class':'row-1'}).appendTo('#board').css(rowCss);
-$('<div>', {'class':'row-2'}).appendTo('#board').css(rowCss);
-$('<div>', {'class':'row-3'}).appendTo('#board').css(rowCss);
-
-// row one * three colums
-$('<div>', {'class':'column-1'}).appendTo('.row-1').css(colCss);
-$('<div>', {'class':'column-2'}).appendTo('.row-1').css(colCss);
-$('<div>', {'class':'column-3'}).appendTo('.row-1').css(colCss);
-
-// row two * three columns
-$('<div>', {'class':'column-1'}).appendTo('.row-2').css(colCss);
-$('<div>', {'class':'column-2'}).appendTo('.row-2').css(colCss);
-$('<div>', {'class':'column-3'}).appendTo('.row-2').css(colCss);
-
-// row three * three colums
-$('<div>', {'class':'column-1'}).appendTo('.row-3').css(colCss);
-$('<div>', {'class':'column-2'}).appendTo('.row-3').css(colCss);
-$('<div>', {'class':'column-3'}).appendTo('.row-3').css(colCss);
+// creating mousehover functions using Jquery
+$('#board').dblclick(function() {
+  $(this).appendTo('#X')
+});
